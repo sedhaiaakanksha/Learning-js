eaa76be941b0e7a04af8bb43f46d932f7b134aff
Aakanksha <sedhaiaakanksha@gmail.com>
2025-07-12 15:54:15 +0545

    ConversionVaiable version 2

--- 01_Basics/03_conversionOperators.js
+++ 01_Basics/03_conversionOperators.js
@@ -38,6 +38,7 @@ Number to Boolean
 String to Number
 " sok" => NAN
 "123" => 123
+" "=>
 */
 
 /*
@@ -45,9 +46,38 @@ Undefined to number => NaN
 Udefined to Boolean => false
 undefined to string => Undefined
 
-
-
 */
+
+//######### OPERATIONS ############ */
+let name="Aakanksha"
+let surname="Sedhai"
+console.log(name+ surname);
+
+let a=12
+let b="11"
+let c= 10
+
+console.log(a +b +c);
+
+
+console.log(typeof (1+3+"2"));
+//If string is the 1st and value then it woud convert everything into string. eg "2"+3+4 output=> 234
+// but, if 1st and 2nd value both are numbers then those two are treated as number and will concat with the string
+
+
+let population=100; 
+console.log(population++);//postfix => increments after printing the value
+console.log(++population);// prefix =>increments before printing the value
+
+
+
+console.log(+true); // converts boolean into number
+console.log( +" ");  // as we know empty string converts into 0 into zero 
+console.log(+false);
+
+
+
+
 
 
 
