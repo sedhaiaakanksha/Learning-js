64b875651dd3f5aa8a62691de9239df7a9ea6e9c
Aakanksha <sedhaiaakanksha@gmail.com>
2025-07-14 17:55:42 +0545

    comaprision operators

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,25 @@
+console.log("2"==2);// == doesnt check its type. It convert the datatype
+console.log("2"===2);// === also checks its types. It doesnt convert datatype.  
+
+console.log(null>0);// our answer will be false because it converts value of null into 0
+console.log(null <0);// our answer will be false because it converts value of null into 0
+console.log(null==0);// converts the datatype
+console.log(null>=0);//>= doesnt change null to zero
+console.log(null<=0);// same with this too
+
+// for undefined everything is false.
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined>=0);
+console.log(undefined <=0);
+console.log(undefined==0);
+
+
+
+
+
+
+
+
+
+
