c2146e752105b25c9e365031dadb0e87ad698377
Aakanksha <sedhaiaakanksha@gmail.com>
2025-08-05 14:42:09 +0545

    Date and Time stamp basics

---
+++ 01_Basics/07_datesAndTime.js
@@ -0,0 +1,34 @@
+let myDate=  new Date();
+// functions of date 
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toString());
+console.log(myDate.toUTCString());
+console.log(myDate.toJSON());
+console.log(myDate.toString());
+
+
+
+
+// let myCreatedDate= new Date(`2082-04-20`);
+// console.log(myCreatedDate.toLocaleString());
+// own date 
+let myCreatedDate= new Date(2082, 1, 10,1,56);
+console.log(myCreatedDate.toLocaleString());
+console.log(myCreatedDate.getMilliseconds());
+console.log(Date.now());
+
+//COnverting millisecond into second
+console.log("Converted into second {");
+console.log(Math.round(Date.now()/1000));
+console.log("}");
+
+
+
+//###### TIMESTAMP ###########
+let myTimeStamp= Date.now();
+
+console.log(myTimeStamp);
+
+
+console.log(myCreatedDate.getTime());
