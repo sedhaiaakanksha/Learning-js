7157c1e328cb7a8c6c0388efe3b50deae87470f9
Aakanksha <sedhaiaakanksha@gmail.com>
2025-08-23 21:39:01 +0545

    JSON

---
+++ 02_Basics/05_objectDestructin.js
@@ -0,0 +1,30 @@
+let employeeInfo={
+    id: "ep1",
+    name: "Employee 1",
+    email: "employee@gmail.com",
+    employeeJoinDate: "Jan-20, 2024"
+}
+
+const {id}= employeeInfo; //object destruction.
+
+const {employeeJoinDate: JoinDate}= employeeInfo; //We can also assign our own variable name.
+
+console.log(JoinDate);
+console.log(id);
+
+
+//API -> API is the task which we assign others to complete.
+
+//JASON and object are kind of similar . The only difference is that JASON API IS WRITTEN  
+// USING BRACES {} or in braces enclosed by brackets.
+// {
+//     "loginId" : 112,
+//     "name": "Aakanksha" 
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
