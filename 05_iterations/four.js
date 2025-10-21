f8bae126814d48ec68720284563698b26ecdcd43
Aakanksha <sedhaiaakanksha@gmail.com>
2025-10-21 16:26:28 +0545

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,22 @@
+//for in loop
+//It is mainly used for object
+
+const myObj={
+    name: "js",
+    fullname: "javascript",
+    platform: "vscode"
+}
+
+for (const key in myObj) {
+   console.log(`${key} :- ${myObj[key]}`);
+   
+}
+//we can also use for in loop in array
+
+const myArr=[1,2,3,4,5,6,7,8,9]
+for(const arr in myArr){
+    // console.log(`Index is ${arr} and elements is ${myArr[arr]}`);
+    console.log(`${myArr[arr]}`);
+    
+    
+}
