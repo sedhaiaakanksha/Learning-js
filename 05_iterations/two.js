8281f1c2ad5c1b4475d1454ee415cb665045f0cb
Aakanksha <sedhaiaakanksha@gmail.com>
2025-10-21 15:39:59 +0545

    basic loops

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -13,6 +13,33 @@ while(index<myArr.length){
     index ++
 }
 
+//break and continue
+//break
+let b=0
+while(b<=10){
+    if (b==5) {
+        console.log("5 is spotted");
+        
+       break 
+    }
+    console.log(`Value of b is ${b}`);
+    b++
+}
+
+
+//Continue
+
+let c=0
+while(c<=10){
+    if (c==2) {
+        c++
+        console.log(`2 is here but its okay`);
+        continue;
+    }
+    console.log(`Value of c is ${c}`);
+     c++
+}
+
 // do while loop
 let j=0
 do{
