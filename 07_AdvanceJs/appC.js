d86d7021947e34f94fa0344985a31229c953a53e
Aakanksha <sedhaiaakanksha@gmail.com>
2026-03-28 18:09:25 +0545

    exporting and importing using module and common js

---
+++ 07_AdvanceJs/appC.js
@@ -0,0 +1,5 @@
+//exporting using common js
+
+const imported = require("./mathOperatorC.js");
+
+console.log(imported.add(6, 8));
---
+++ 07_AdvanceJs/appM.js
@@ -0,0 +1,9 @@
+//importing function using module
+
+import multiply from "./mathOperatorM.js";
+
+import { add, subtract } from "./mathOperatorM.js";
+
+console.log(multiply(2, 5));
+
+console.log(add(89, 23));
